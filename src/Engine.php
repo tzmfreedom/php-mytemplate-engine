6978b7eb89dbcc496d31fef68d6ec278f25dbfda
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-06-02 17:27:21 +0900

    add cache

--- src/Engine.php
+++ src/Engine.php
@@ -5,10 +5,17 @@ namespace MyTemplate;
 class Engine
 {
     /**
-     * Engine constructor.
+     * @var string
      */
-    public function __construct()
+    private $cacheDir;
+
+    /**
+     * Engine constructor.
+     * @param string $cacheDir
+     */
+    public function __construct(string $cacheDir = './cache')
     {
+        $this->cacheDir = $cacheDir;
     }
 
     /**
@@ -19,6 +26,11 @@ class Engine
      */
     public function render($filePath, $params = [])
     {
+        $cachePath = $this->getCacheFilePath($filePath);
+        if (file_exists($cachePath)) {
+            $this->renderFromCache($cachePath, $params);
+            return;
+        }
         $src = file_get_contents($filePath);
         $this->renderString($src, $params);
     }
@@ -37,12 +49,48 @@ class Engine
     }
 
     /**
+     * @param string $filePath
+     * @param string|null $cachePath
+     * @throws EofException
+     * @throws SyntaxError
+     */
+    public function compile(string $filePath, ?string $cachePath = null)
+    {
+        if ($cachePath === null) {
+            $cachePath = $this->getCacheFilePath();
+        }
+        $src = file_get_contents($filePath);
+        $code = $this->generateCode($src);
+        file_put_contents($cachePath, $code);
+    }
+
+    /**
+     * @param string $cacheFilePath
+     * @param array $params
+     */
+    private function renderFromCache(string $cacheFilePath, array $params)
+    {
+        $_code = file_get_contents($cacheFilePath);
+        extract($params);
+        eval($_code);
+    }
+
+    /**
+     * @param $filePath
+     * @return string
+     */
+    private function getCacheFilePath($filePath): string
+    {
+        return $this->cacheDir . '/' . basename($filePath);
+    }
+
+    /**
      * @param $src
      * @return string
      * @throws EofException
      * @throws SyntaxError
      */
-    private function generateCode($src)
+    private function generateCode(string $src): string
     {
         $lexer = new Lexer($src);
         $tokens = $lexer->parse();
